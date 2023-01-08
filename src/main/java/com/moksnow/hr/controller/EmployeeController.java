e42cf2201d2f28213de3ce6855d52e07a58d1838
moksnow <khandanmoh@gmail.com>
2023-01-08 23:07:46 +0330

    cross origin

--- src/main/java/com/moksnow/hr/controller/EmployeeController.java
+++ src/main/java/com/moksnow/hr/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+//@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
