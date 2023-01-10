370b3609b337ed8807accc22fe11ee0b7305634e
moksnow <khandanmoh@gmail.com>
2023-01-10 20:48:22 +0330

    add header and footer components

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 
 function App() {
   return (
-    <div className="container">
-      <ListEmployeeComponent />
+    <div>
+
+    <HeaderComponent/>
+      <div className="container">
+        <ListEmployeeComponent />
+      </div>
+    <FooterComponent/>
     </div>
   );
 }
---
+++ app/src/components/HeaderComponent.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class HeaderComponent extends Component {
+
+constructor(props){
+    super(props)
+
+    this.state = {
+
+    }
+}
+
+    render() {
+        return (
+            <div>
+                    <header>
+                        <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
+                            <div>
+                                <a href='http://google.com' className='navbar-brand'>HR app</a>
+                            </div>
+                        </nav>
+                    </header>
+            </div>
+        );
+    }
+}
+
+export default HeaderComponent;
